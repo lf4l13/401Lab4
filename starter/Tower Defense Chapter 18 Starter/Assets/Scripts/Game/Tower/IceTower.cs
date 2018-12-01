0bbcd3b1829d204fade712601647a5d5fa334c11
lf4l13 <lf4l@hotmail.ca>
2018-11-30 20:51:46 -0500

    Ice tower, fire tower and VR stuff

---
+++ starter/Tower Defense Chapter 18 Starter/Assets/Scripts/Game/Tower/IceTower.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceTower : Tower {
+
+    public GameObject icePrefab;
+
+    public override void Update()
+    {
+        base.Update();
+        GetNonFrozenTarget();
+    }
+
+    private void GetNonFrozenTarget()
+    {
+        foreach (Enemy enemy in GetEnemiesInAggroRange())
+        {
+            if (!enemy.frozen)
+            {
+                targetEnemy = enemy;
+                break;
+            }
+        }
+    }
+    protected override void AttackEnemy()
+    {
+        base.AttackEnemy();
+        
+        GameObject ice = (GameObject)Instantiate(icePrefab, towerPieceToAim.position, Quaternion.identity);
+        ice.GetComponent<Followingprojectile>().enemyToFollow = targetEnemy;
+    }
+}
---
+++ starter/Tower Defense Chapter 18 Starter/Assets/Scripts/Game/Tower/Weapons/Ice.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ice : Followingprojectile {
+
+    protected override void OnHitEnemy()
+    {
+        enemyToFollow.Freeze();
+        Destroy(gameObject);
+    }
+
+}
